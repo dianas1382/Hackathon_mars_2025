aebd8ae6a21b2fb18cd45d6681bc5729a2852896
seb <sfv.jacob@gmail.com>
2025-03-15 10:34:07 +0100

    Add user accounts

--- src/main.py
+++ src/main.py
@@ -3,15 +3,17 @@ import dotenv
 import boto3
 import json
 import base64
-from flask import Flask, request, render_template
+import sqlite3
+import jwt
+from flask import Flask, request, render_template, Response
 
 
 dotenv.load_dotenv(".env", override=True)
 
-
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = os.environ.get('AWS_REGION')
+JWT_SECRET = os.environ.get('JWT_SECRET')
 
 bedrock_runtime_client = boto3.client(
     'bedrock-runtime',
@@ -40,6 +42,50 @@ def home():
 
 
 
+@app.route('/my')
+def my():
+    token = request.cookies.get("token")
+    try:
+        d = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+    except:
+        return "unauthorized"
+    return d['user']
+
+@app.route('/signin', methods=["GET","POST"])
+def sign_in():
+    if request.method == "GET":
+        return render_template("signin.html")
+    usercon = sqlite3.connect("user.db")
+    usercur = usercon.cursor()
+    user = request.form["user"]
+    password = request.form["password"]
+    if not user.isalnum() or not password.isalnum():
+        return "Fields should only contain letters  and digits"
+    res = usercur.execute(f"SELECT name, password FROM user WHERE name='{user}'AND password='{password}'").fetchone()
+    if res is None:
+        return "Not Found"
+    resp = Response()
+    resp.headers["Set-Cookie"] = f"token={jwt.encode({"user": user}, JWT_SECRET, algorithm="HS256")}; HttpOnly; Secure; SameSite=Strict"
+    return resp
+
+@app.route('/signup', methods=["GET","POST"])
+def sign_up():
+    if request.method == "GET":
+        return render_template("signup.html")
+    usercon = sqlite3.connect("user.db")
+    usercur = usercon.cursor()
+    user = request.form["user"]
+    password = request.form["password"]
+    if not user.isalnum() or not password.isalnum():
+        return "Fields should only contain letters and digits"
+    res = usercur.execute(f"SELECT name, password FROM user WHERE name='{user}'").fetchone()
+    if res is not None:
+        return "User already exists"
+    usercur.execute(f"""INSERT INTO user VALUES
+        ('{user}', '{password}')""")
+    usercon.commit()
+    return "YAY"
+
 
 def getImageDescription(encoded_image):
     payload = {
@@ -57,7 +103,7 @@ def getImageDescription(encoded_image):
                     },
                     {
                         "type": "text",
-                        "text": "What are the ingredients of the image?"
+                        "text": "What recipes can we make with the ingredients of the image?"
                     }
                 ]
             }
